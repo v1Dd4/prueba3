5adc75416d6245cacf00af252fd5a7a6d25542c5
David <VIDDA.REDEYES@GMAIL.COM>
2023-07-10 15:07:14 -0400

    inicios de prueba examen

--- function.py
+++ function.py
@@ -2,15 +2,14 @@ import os
 import time as tm
 import numpy as np
 
-array_asientos = np.zeros((7,6))
-
 
 lista_datos = []
 
 def comprar_asiento():
-    numasiento = input('INGRESE EL NUMERO DE ASIENTO QUE DESEA COMPRAR')
+    numasiento = input('INGRESE EL NUMERO DE ASIENTO QUE DESEA COMPRAR\n')
     nombrePasajero = input('INGRESE EL NOMBRE DEL PASAJERO\n') 
     rutPasajero = input('INGRESE EL RUT DEL PASAJERO\n') 
     telefonoPasajero = input('INGRESE EL TELEFONO DEL PASAJERO\n+56')
     bancoPasajero = input('INGRESE EL BANCO DEL PASAJERO\n')
-    datosuser = [numasiento]
+    datosuser = [numasiento,nombrePasajero,rutPasajero,telefonoPasajero,bancoPasajero]
+    lista_datos.append[datosuser]
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import os
 import time as tm
 import numpy as np
+from function import comprar_asiento
+cont = 0
+array_asientos = np.zeros((7,6))
+for i in range(7):
+    for j in range(6):
+        cont += 1
+        array_asientos[i][j] = cont
